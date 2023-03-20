ca398a64442ac221903d67f8cb95919d47b6caf0
jlb86340 <math86340@orange.fr>
2023-03-20 17:17:26 +0100

    4.2

--- src/Controller/Sandbox/FormController.php
+++ src/Controller/Sandbox/FormController.php
@@ -7,6 +7,7 @@ use App\Form\Sandbox\FilmType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
@@ -36,4 +37,38 @@ class FormController extends AbstractController
 
         return $this->render('Sandbox/Form/film_edit.html.twig', $args);
     }
+
+    #[Route(
+        '/film/editbis/{id}',
+        name: '_film_editbis',
+        requirements: ['id' => '[1-9]\d*'],
+    )]
+    public function filmEditbisAction(int $id, EntityManagerInterface $em, Request $request): Response
+    {
+        $filmRepository = $em->getRepository(Film::class);
+        $film = $filmRepository->find($id);
+
+        if (is_null($film))
+            throw new NotFoundHttpException('film ' . $id . ' inexistant');
+
+        $form = $this->createForm(FilmType::class, $film);
+        $form->add('send', SubmitType::class, ['label' => 'edit film']);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $em->flush();
+            $this->addFlash('info', 'édition film réussie');
+            return $this->redirectToRoute('sandbox_doctrine_critique_view2', ['id' => $film->getId()]);
+        }
+
+        if ($form->isSubmitted())
+            $this->addFlash('info', 'formulaire film incorrect');
+
+        $args = array(
+            'myform' => $form->createView(),
+        );
+        return $this->render('Sandbox/Form/film_editbis.html.twig', $args);
+    }
 }
+

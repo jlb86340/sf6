e54cfac5bd912e17759cc44448a3df5f9d6ec067
jlb86340 <math86340@orange.fr>
2023-03-20 16:34:32 +0100

    4

--- src/Form/Sandbox/FilmType.php
+++ src/Form/Sandbox/FilmType.php
@@ -4,6 +4,10 @@ namespace App\Form\Sandbox;
 
 use App\Entity\Sandbox\Film;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,11 +16,31 @@ class FilmType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('titre')
-            ->add('annee')
-            ->add('enstock')
-            ->add('prix')
-            ->add('quantite')
+            ->add('titre',
+                TextType::class, //déduit automatiquement par symfony
+                [
+                    'label' => 'titre du film',
+                    'attr' => ['placeholder' => 'titre'],
+                ])
+            ->add('annee',
+                IntegerType::class, // déduit automatiquement par Symfony
+                ['label'=>'année de sortie'])
+            ->add('enstock',
+                ChoiceType::class, // par défaut c'est CheckboxType
+                [
+                    'label' => 'en stock',
+                    'choices' => ['oui' => true, 'non' => false],  // liste des choix : labels et valeurs
+                    'expanded' => true,                            // liste déroulante ou radio-boutons
+                ])
+            ->add('prix',
+                NumberType::class,     // déduit automatiquement par Symfony
+                ['label' => 'prix d\'achat'])
+            ->add('quantite',
+                IntegerType::class,    // déduit automatiquement par Symfony
+                [
+                    'label' => 'quantité en stock',
+                    'help' => '0 si "enstock" est à "non"',           // message d'aide lié au champ
+                ])
         ;
     }
 

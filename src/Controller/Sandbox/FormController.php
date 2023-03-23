dcd34973183f785d9d0fb1aae7dce6e974da78ce
jlb86340 <math86340@orange.fr>
2023-03-23 17:24:23 +0100

    6.2

--- src/Controller/Sandbox/FormController.php
+++ src/Controller/Sandbox/FormController.php
@@ -80,10 +80,11 @@ class FormController extends AbstractController
             ->setTitre(str_repeat('abc', 100))      // trop de caractères
             ->setAnnee(1849)                        // année trop petite
             ->setEnstock(true)
-            ->setPrix(0.99)                             // prix trop faible
+            ->setPrix(0.99)                         // prix trop faible
             ->setQuantite(-15)                      // incohérent avec enstock (callback), quantité négative
         ;
         dump($validator->validate($film));
         return new Response('<body>cf. dump</body>');
     }
 }
+
